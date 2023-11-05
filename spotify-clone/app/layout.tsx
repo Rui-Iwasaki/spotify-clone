69102da9ad434fcd7d8f13fde7802e812a2a95db
John <mpfjd077@yahoo.co.jp>
2023-11-06 01:11:46 +0900

    sidebar1

---
+++ spotify-clone/app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import Sidebar from '@/components/Sidebar'
+import { Figtree } from 'next/font/google'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Spotify Clone',
+  description: 'Listen to music!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Sidebar>
+          {children}
+        </Sidebar>
+        {children}
+        </body>
+    </html>
+  )
+}

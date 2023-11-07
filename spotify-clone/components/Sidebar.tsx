1a31315dca2e30bfd46f4c411336c7f43528ffa2
John <mpfjd077@yahoo.co.jp>
2023-11-08 02:20:07 +0900

    add hover function

--- spotify-clone/components/Sidebar.tsx
+++ spotify-clone/components/Sidebar.tsx
@@ -6,6 +6,8 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+
 interface SidebarProps{
     children: React.ReactNode
 }
@@ -45,9 +47,24 @@ const Sidebar: React.FC<SidebarProps> = ({
     "
     >
         <Box>
-            Sidebar Navigation
+            <div
+                className="
+                    flex
+                    flex-col
+                    gap-y-4
+                    px-5
+                    py-4
+                "
+            >
+                {routes.map((item) => (
+                    <SidebarItem
+                        key={item.label}
+                        {...item}
+                    />
+                ))}
+            </div>
         </Box>
-        <Box>
+        <Box className="overflow-y-auto h-full">
             Song Library
         </Box>
 
---
+++ spotify-clone/components/SidebarItem.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { IconType } from "react-icons";
+import { twMerge } from "tailwind-merge";
+
+interface SidebarItemProps {
+    icon: IconType;
+    label: string;
+    active?: boolean;
+    href: string;
+}
+
+const  SidebarItem: React.FC<SidebarItemProps> = ({
+    icon,
+    label,
+    active,
+    href
+}) => {
+    return (
+        <Link
+            href={href}
+            className={twMerge(`
+                flex
+                flex-row
+                h-auto
+                items-center
+                w-full
+                gap-x-4
+                text-md
+                font-midium
+                cursor-pointer
+                hover:text-white
+                transition
+                text-neutral-400
+                py-1
+            `)}
+        >
+            Sidebar Item
+        </Link>
+        );
+    }
+
+    export default SidebarItem;

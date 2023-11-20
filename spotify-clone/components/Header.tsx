3d25b5dbc82c07daa03616bf94199be008961ce4
John <mpfjd077@yahoo.co.jp>
2023-11-21 06:33:08 +0900

    Add SideBar AutoAdjust(Show/Hide) Function

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
-
+import { RxCaretLeft } from "react-icons/rx";
 interface HeaderProps {
     children: React.ReactNode;
     className?: string;
@@ -19,14 +19,33 @@ const Header: React.FC<HeaderProps> = ({
      
     return ( 
         <div
-        className={twMerge(`
-        h-fit
-        bg-gradient-to-b
-        from-emerald-800
-        p-6
-        `)}
+            className={twMerge(`
+            h-fit
+            bg-gradient-to-b
+         from-emerald-800
+            p-6
+           `,
+             className
+            )}
         >
-            
+         <div className="
+         w-full
+         mb-4
+         flex
+         items-center
+         justify-bitween
+         ">
+           <div className="
+             hidden
+             md:flex
+             gap-x-2
+             items-center
+           ">
+            <button>
+                <RxCaretLeft />
+            </button>
+          </div>
+         </div>
         </div>
      );
 }

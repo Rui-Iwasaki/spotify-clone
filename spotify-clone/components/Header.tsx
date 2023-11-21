57a5795d03b4b3f55ec3f03ab0237a2a0a503034
John <mpfjd077@yahoo.co.jp>
2023-11-22 06:29:31 +0900

    Add back/forward Button

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -2,7 +2,8 @@
 
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
-import { RxCaretLeft } from "react-icons/rx";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+
 interface HeaderProps {
     children: React.ReactNode;
     className?: string;
@@ -41,8 +42,33 @@ const Header: React.FC<HeaderProps> = ({
              gap-x-2
              items-center
            ">
-            <button>
-                <RxCaretLeft />
+            <button
+            onClick={() => router.back()}
+             className="
+             rounded-full
+             bg-black
+             flex
+             items-center
+             justify-center
+             hover:opacity-75
+             transition
+             "
+            >
+                <RxCaretLeft className="text-white" size={35} />
+            </button>
+            <button
+            onClick={() => router.forward()}
+             className="
+             rounded-full
+             bg-black
+             flex
+             items-center
+             justify-center
+             hover:opacity-75
+             transition
+             "
+            >
+                <RxCaretRight className="text-white" size={35} />
             </button>
           </div>
          </div>
